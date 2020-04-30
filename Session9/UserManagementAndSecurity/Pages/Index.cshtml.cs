65357ec1fccbdfcae09870e07cd8b656b7be985a
VIA-JH <48440436+VIA-Jesper@users.noreply.github.com>
2020-04-30 21:22:37 +0200

    Session 9 Exercises minus DB and reg

---
+++ Session9/UserManagementAndSecurity/Pages/Index.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace UserManagementAndSecurity.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPostLoginAsync(string name)
+        {
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, name)
+            };
+
+            // simple method
+            if (name.Equals("123"))
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "admin"));
+            };
+            if (name.Equals("321"))
+            {
+                claims.Add(new Claim(ClaimTypes.Role, "student"));
+            }
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+
+            return RedirectToPage("/Index");
+        }
+
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
